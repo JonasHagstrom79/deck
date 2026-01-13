a31a9b11bf431a0974e219aeb4aafdc17b18109f
JonasHagstrom79 <carl.jonas.hagstrom@gmail.com>
2026-01-13 19:12:36 +0100

    added card creator loop

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,17 @@ struct Deck {
 }
 
 fn main() {
+
+    let suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+    let ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+    let mut cards = vec![];
+
+    for suit in suits {
+        for rank in ranks {
+            let card = format!("{} of {}", rank, suit);
+            cards.push(card);
+        }
+    }
     let deck: Deck = Deck { cards: vec![] };
     println!("Here is your deck: {:?}", deck);
 }
